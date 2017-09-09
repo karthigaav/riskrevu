1c1d5d20062989056b7b1765b1313c84167c772f
karthigaav <karthigaav@my.ccsu.edu>
2017-09-09 18:44:43 -0400

    vfdvdvvvcvdv

--- site_admin/application/views/dashboard/main.php
+++ site_admin/application/views/dashboard/main.php
@@ -85,33 +85,6 @@
                                         </a>
                                     </li>
                                     <!-- end message -->
-                                    <li>
-                                        <a href="#">
-                                            <div class="pull-left">
-                                                <img src="dist/img/user3-128x128.jpg" class="img-circle"
-                                                     alt="User Image">
-                                            </div>
-                                            <h4>
-                                                Message from individual contributors 
-                                                <small><i class="fa fa-clock-o"></i> 2 hours</small>
-                                            </h4>
-                                            <p>Why not buy a new awesome theme?</p>
-                                        </a>
-                                    </li>
-                                    
-                                    <li>
-                                        <a href="#">
-                                            <div class="pull-left">
-                                                <img src="dist/img/user4-128x128.jpg" class="img-circle"
-                                                     alt="User Image">
-                                            </div>
-                                            <h4>
-                                                Reviewers
-                                                <small><i class="fa fa-clock-o"></i> 2 days</small>
-                                            </h4>
-                                            <p>Why not a help information?</p>
-                                        </a>
-                                    </li>
                                 </ul>
                             </li>
                             <li class="footer"><a href="#">See All Messages</a></li>
