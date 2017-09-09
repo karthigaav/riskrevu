15ea02a66be02bdc4ae3f313178bb69600f5cef8
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-09-09 13:21:12 -0400

    htaccess

--- site_admin/application/config/config.php
+++ site_admin/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://codeigniter-tutorial-msrinteractive.c9users.io/';
+$config['base_url'] = 'https://www.riskrevu.com/site_admin';
 
 /*
 |--------------------------------------------------------------------------
