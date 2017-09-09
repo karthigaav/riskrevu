80c97a56bd4959574caa94f613f0a30f458f0126
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-09-09 14:40:14 -0400

    started agent view

--- site_admin/application/controllers/Dashboard.php
+++ site_admin/application/controllers/Dashboard.php
@@ -2,21 +2,26 @@
 
     public function __construct(){
         parent::__construct();
+        $this->load->model('clientform_model');
     }
 
     public function index(){
         $this->load->view('dashboard/main');
     }
 
-    public function get_clients(){
+    public function agent_view(){
         // get data from the database first,
         // pass data to the view
         // load the view
+        $clientlist = $this->clientform_model->get_all();
 
+        $this->data['clientlist'] = $clientlist;
+
+        $this->load->view('dashboard/agentview', $this->data);
 
     }
 
-    public function set_clients(){
+    public function agent_login(){
         // get data from the user input
         // insert or update on the database
         // load a view / or return values, load a view
