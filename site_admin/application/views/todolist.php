a0aaa36d716bd57fb87b2fd3ebf750a7c5d54363
KARTHIGAA VIJAYAKUMAR <karthigaav@my.ccsu.edu>
2017-09-09 11:33:23 -0400

    test

--- site_admin/application/views/todolist.php
+++ site_admin/application/views/todolist.php
@@ -71,6 +71,8 @@
     
     <!--CONTENT ENDS -->
 </body>
+        <!--JavaScript-->
+
 <script src="<?php echo site_url('assets/jquery/jquery-3.2.1.min.js');?>"></script>
 <script src="<?php echo site_url('assets/bootstrap-3.3.7-dist/js/bootstrap.min.js');?>"></script>
 <script src="<?php echo site_url('assets/wow/dist/wow.min.js');?>"></script>
