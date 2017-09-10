359e70904ed729f5aa6307a5706a2479ca5b9e80
karthigaav <karthigaav@my.ccsu.edu>
2017-09-09 20:55:09 -0400

    hellobjbbvcd

--- site_admin/application/views/dashboard/main.php
+++ site_admin/application/views/dashboard/main.php
@@ -267,9 +267,11 @@
         <section class="sidebar">
             <!-- Sidebar user panel -->
             <div class="user-panel">
+            <!--
                 <div class="pull-left image">
                     <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                 </div>
+                -->
                 <div class="pull-left info">
                     <p>Curt Thoennessen</p>
                     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
