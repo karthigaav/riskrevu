ed4009d18ceb31bf4ba6b0a0b85b0b23ea527eb6
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-09-09 22:12:56 -0400

    commit

--- site_admin/application/views/dashboard/main.php
+++ site_admin/application/views/dashboard/main.php
@@ -220,13 +220,13 @@
                     <!-- User Account: style can be found in dropdown.less -->
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+
                             <span class="hidden-xs">Alexander Pierce</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
                             <li class="user-header">
-                                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
 
                                 <p>
                                     Alexander Pierce - Web Developer
@@ -274,11 +274,11 @@
             <!-- Sidebar user panel -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
                 </div>
                 <div class="pull-left info">
-                    <p>Curt Thoennessen</p>
-                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+
+
                 </div>
             </div>
             <!-- search form -->
